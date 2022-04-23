fa339195c9476fc6756d77973e4838e0d0add0e8
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2022-04-23 22:38:46 +0200

    :))

--- utilities.js
+++ utilities.js
@@ -17,43 +17,13 @@ function msToTime(s) {
     if (hrs > 0) result += hrs + " hod. ";
     if (mins > 0) result += mins + " min.";
 
-    return "Odpracováno: " + result;
+    return result;
   }
 
 
   function validateForm() {
     let form = $("#form");
-    let date = $("#date").val();
-
-    let start_date = $("#start-date").val();
-    let start_time = $("#start-time").val();
-
-    let end_date = $("#end-date").val();
-    let end_time = $("#end-time").val();
-
-
-    if (end_time === "" || start_time === ""){
-        alert("Chybí čas");
-        return false;
-    }
-    if (end_date === "" || start_date === ""){
-        alert("Chybí datum");
-        return false;
-    }
-    let start_mili = new Date(start_date + " " + start_time).getTime()
-    let end_mili = new Date(end_date + " " + end_time).getTime()
-    console.log(start_mili)
-    console.log(end_mili)
-    console.log(start_mili >= end_mili)
-    if(start_mili >= end_mili){
-        alert("datum musí být v budoucnosti!");
-        return false;
-    }
-
-      if (date == "") {
-        alert("Datum musí být vyplněno");
-        return false;
-      }
+    
   }
 
 
@@ -82,7 +52,75 @@ function msToTime(s) {
     } else {
         difference = msToTime(end_mili - start_mili);
         console.log("diff: " + difference);
-        document.getElementById("time-calculation").innerHTML="<p>"+difference+"</p>";
+        document.getElementById("time-calculation").innerHTML="<p>Odpracováno: <b>"+difference+"</b></p>";
+        $("#time-spent").attr("value", difference);
     }
 }
+
+
+function addToLog(data, form){
+    var status = false;
+    if(data === "Accepted"){
+        console.log("ok");
+        var status = true;
+    }
+
+    var order_number = $("#order-number").val();
+    var worker = $("#worker").val();
+    var operation = $("#work-type").val();
+    var machine = $("#work-machine").val();
+    var now = new Date(new Date().getTime()).toLocaleTimeString()
+
+    document.getElementById("log").innerHTML += `<li>${now} - ${order_number} - ${worker} - ${operation}, ${machine}</li>`  
+
+
+}
+
+$("#form").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(this);
+    var actionUrl = "https://hook.eu1.make.com/j93ll3zxaiqt5ws7nea5cxbzxesiomun";
+    let date = $("#date").val();
+
+    let start_date = $("#start-date").val();
+    let start_time = $("#start-time").val();
+
+    let end_date = $("#end-date").val();
+    let end_time = $("#end-time").val();
+
+
+    if (end_time === "" || start_time === ""){
+        alert("Chybí čas");
+        return false;
+    }
+    if (end_date === "" || start_date === ""){
+        alert("Chybí datum");
+        return false;
+    }
+    let start_mili = new Date(start_date + " " + start_time).getTime()
+    let end_mili = new Date(end_date + " " + end_time).getTime()
+    console.log(start_mili >= end_mili)
+    if(start_mili >= end_mili){
+        alert("datum musí být v budoucnosti!");
+        return false;
+    }
+
+    if (date == "") {
+        alert("Datum musí být vyplněno");
+        return false;
+    }
+    
+    $.ajax({
+        type: "POST",
+        url: actionUrl,
+        data: form.serialize(), // serializes the form's elements.
+        success: function(data)
+        {
+          addToLog(data, form);
+        }
+    });
+    
+});
 
