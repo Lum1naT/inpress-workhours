aa0179bc10111162df641d0b8f2d25e9c310fb3a
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2022-04-24 13:26:26 +0200

    relinked sheets

--- machines.js
+++ machines.js
@@ -1,32 +1,28 @@
-var id = '1a6EElxcMa7kGsiWU8oYtK94IwWV8FHhq_rBHt9Zl2o8';
-var gid = '472944468';
-var url = 'https://docs.google.com/spreadsheets/d/'+id+'/gviz/tq?tqx=out:json&tq&gid='+gid;
+var id = '16HbuwE9wJDuA6pb1SaEzqnn9Dljo6N8v';
+var gid = '1500073696';
+var url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?tqx=out:json&tq&gid=' + gid;
 fetch(url)
-  .then(response => response.text())
-  .then(data => document.getElementById("machines").innerHTML=myItems(data.substring(47).slice(0, -2))  
-  );
-  function myItems(jsonString){
+    .then(response => response.text())
+    .then(data => document.getElementById("machines").innerHTML = myItems(data.substring(47).slice(0, -2)));
+
+function myItems(jsonString) {
     var json = JSON.parse(jsonString);
     var first = true;
     var result = ''
     json.table.rows.forEach(line => {
-      var temp = ''
-      line.c.forEach(cell => {
-          try{var value = cell.f ? cell.f : cell.v}
-          catch(e){var value = ''}
-          temp += value + " "
+        var temp = ''
+        line.c.forEach(cell => {
+            try { var value = cell.f ? cell.f : cell.v } catch (e) { var value = '' }
+            temp += value + " "
+        })
+        temp = temp.trimEnd()
+        result += `<option value="${temp}">`
+
+        if (first) {
+            result = "";
+            first = false;
         }
-      )
-      temp = temp.trimEnd()
-      result += `<option value="${temp}">`
 
-      if (first) {
-        result = "";
-        first = false;
-      }
-
-    }
-    )
+    })
     return result
-  }
-
+}
