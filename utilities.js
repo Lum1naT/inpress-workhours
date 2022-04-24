87e0f08cbbb2cdfde1ffe8718f917e321563904a
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2022-04-24 13:09:04 +0200

    adjustments

--- utilities.js
+++ utilities.js
@@ -1,7 +1,7 @@
 Date.prototype.toDateInputValue = (function() {
     var local = new Date(this);
     local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-    return local.toJSON().slice(0,10);
+    return local.toJSON().slice(0, 10);
 });
 
 function msToTime(s) {
@@ -11,33 +11,27 @@ function msToTime(s) {
     s = (s - secs) / 60;
     var mins = s % 60;
     var hrs = (s - mins) / 60;
-    
+
     result = "";
 
     if (hrs > 0) result += hrs + " hod. ";
     if (mins > 0) result += mins + " min.";
 
     return result;
-  }
+}
 
 
-  function validateForm() {
-    let form = $("#form");
-    
-  }
-
-
-  function calculateTimeSpent(){
+function calculateTimeSpent() {
     let start_date = $("#start-date").val();
     let start_time = $("#start-time").val();
 
     let end_date = $("#end-date").val();
     let end_time = $("#end-time").val();
-    if (end_time === "" || start_time === ""){
+    if (end_time === "" || start_time === "") {
         console.log("no time");
         return "";
     }
-    if (end_date === "" || start_date === ""){
+    if (end_date === "" || start_date === "") {
         console.log("no date");
         return "";
     }
@@ -46,22 +40,22 @@ function msToTime(s) {
     console.log(start_mili)
     console.log(end_mili)
     console.log(start_mili >= end_mili)
-    if(start_mili >= end_mili){
-        document.getElementById("time-calculation").innerHTML="<p> Datum musí být v budoucnosti </p>";
+    if (start_mili >= end_mili) {
+        document.getElementById("time-calculation").innerHTML = "<p> Datum musí být v budoucnosti </p>";
         return "";
     } else {
         difference = msToTime(end_mili - start_mili);
         console.log("diff: " + difference);
-        document.getElementById("time-calculation").innerHTML="<p>Odpracováno: <b>"+difference+"</b></p>";
+        document.getElementById("time-calculation").innerHTML = "<p>Odpracováno: <b>" + difference + "</b></p>";
         $("#time-spent").attr("value", difference);
         return difference
     }
 }
 
 
-function addToLog(data, form){
+function addToLog(data, form) {
     var status = false;
-    if(data === "Accepted"){
+    if (data === "Accepted") {
         console.log("ok");
         var status = true;
     }
@@ -72,7 +66,8 @@ function addToLog(data, form){
     var machine = $("#work-machine").val();
     var now = new Date(new Date().getTime()).toLocaleTimeString()
 
-    document.getElementById("log").innerHTML += `<li>${now} - ${order_number} - ${worker} - ${operation}, ${machine}</li>`  
+    document.getElementById("log").innerHTML += `<li>${now} - ${order_number} - ${worker} - ${operation}, ${machine}</li>`
+    alert(`Formluář pro pracovníka ${worker} byl odeslán`);
 
 
 }
@@ -92,18 +87,18 @@ $("#form").submit(function(e) {
     let end_time = $("#end-time").val();
 
 
-    if (end_time === "" || start_time === ""){
+    if (end_time === "" || start_time === "") {
         alert("Chybí čas");
         return false;
     }
-    if (end_date === "" || start_date === ""){
+    if (end_date === "" || start_date === "") {
         alert("Chybí datum");
         return false;
     }
     let start_mili = new Date(start_date + " " + start_time).getTime()
     let end_mili = new Date(end_date + " " + end_time).getTime()
     console.log(start_mili >= end_mili)
-    if(start_mili >= end_mili){
+    if (start_mili >= end_mili) {
         alert("datum musí být v budoucnosti!");
         return false;
     }
@@ -115,16 +110,14 @@ $("#form").submit(function(e) {
 
     $("#time-spent").attr("value", calculateTimeSpent());
 
-    
+
     $.ajax({
         type: "POST",
         url: actionUrl,
         data: form.serialize(), // serializes the form's elements.
-        success: function(data)
-        {
-          addToLog(data, form);
+        success: function(data) {
+            addToLog(data, form);
         }
     });
-    
-});
 
+});
